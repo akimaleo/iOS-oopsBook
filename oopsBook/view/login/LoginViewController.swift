72fc4952187d0d8bf7f17984df543921b44b8f3a
kawa <akimaleo@ya.ru>
2018-02-03 23:51:31 +0200

    Added a segue to main part of application from login page if user is authorized by google account. Also created a TabViewController for 3 tabs. Icon defined.

---
+++ oopsBook/view/login/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  oopsBook
+//
+//  Created by Yaroslav Movchan on 03.02.2018.
+//  Copyright © 2018 Yaroslav Movchan. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+class ViewController: UIViewController, GIDSignInUIDelegate {
+    
+    @IBOutlet weak var oauthGoogleButton: GIDSignInButton!
+    
+    @IBAction func onOauthGoogle(_ sender: Any) {
+        oauthGoogleButton.sendActions(for: .touchUpInside)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //IS AUTHORIZED BY GOOGLE
+        if GIDSignIn.sharedInstance().hasAuthInKeychain(){
+            launchMainActivity()
+        }
+    }
+    private func launchMainActivity(){
+        var mainView: UIStoryboard!
+        mainView = UIStoryboard(name: "MainStoryboard", bundle: nil)
+        print("tut")
+        if let viewcontroller = mainView.instantiateViewController(withIdentifier: "main") as? UITabBarController {
+            print("in if let")
+            self.present(viewcontroller, animated: true, completion: nil)
+        }
+    }
+}
